c8bd2679948388a073b17ec4f7ce4e3844aacfc3
Alexandr-77 <62809713+Alexandr-77@users.noreply.github.com>
2023-06-20 12:36:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// 
+// ================================================== 
+let burger = document.querySelector('.burger');
+let nav = document.querySelector('.header__block-nav');
+let liHeader = document.querySelectorAll('.header__menu-item');
+
+burger.addEventListener('click', rotateBurger); 
+
+for(let i = 0; i < liHeader.length; i++ ) {
+  liHeader[i].addEventListener('click', rotateBurger);
+}
+
+
+function rotateBurger() { 
+  burger.classList.toggle('burger-active');
+
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
+}
+
+// ===========================================
+// отправка формы =============>
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Спасибо. Ваша заявка была отправлена. Наш менеджер свяжется с вами в течение 24 часов.");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
+// ============================
+// ===============================================
+
+$('.slider__box').slick({
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+
+  responsive: [
+	{
+	  breakpoint: 1500,
+	  settings: {
+		arrows: false,
+	  }
+	},
+
+ ]
+
+});
